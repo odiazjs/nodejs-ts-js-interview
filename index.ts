2482cb25da70e51a8c5b398275d4137e48bad787
odiazjs <odiaz.dw@gmail.com>
2022-08-09 13:31:41 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -3,27 +3,47 @@
  * For example if given the string 'ReVERt this StriNG' the expected output
  * should be 'rEverT THIS sTRIng'
  * Conditions: Do not use a for loop or the .forEach utility function,
- * you must use the .map() array operator.
+ * you must use the .map() Array operator. Also filter() these characters ['.', '/']
  */
 
-const sText = 'LETS INVERT the CasE in THIS sTRING';
+const sentence = 'LETS . / INVERT the CasE . / in THIs . // sTRING';
 const result = [];
-const invertString = (char) => {
+const invertCasing = (letter) => {
   // your code here
 };
 
 /** ASYNCHRONOUS PROGRAMMING
- * 2.) Given a function doAsync(), write code inside this function
- * that logs to the console a parameter called 'word' from an outside function after 2 seconds
+ * 2.) Given a function doAsync(), write code in this
+ * function that executes a passed function that prints
+ * to the console a parameter called 'word' from an
+ * outside function after 2 seconds
  */
 
 const doAsync = (/** ? */) => {
   // your code here
 };
 
+/** CLOSURES
+ * 3.) make the console.log inside the function outter()
+ * print 'Bob' instead of 'John'
+ */
+function outter() {
+  this.name = 'John';
+  return (function () {
+    console.log(this.name);
+  })();
+}
+
+function closure() {
+  this.name = 'Bob';
+  // your code here
+}
+
+closure();
+
 /** OBJECT ORIENTED PROGRAMMING
- * 3.) Write a typescript class Robot that is generic so that you can
- * create varios Robots of different companies [SpaceX, DynamicIndustries, GenericProtos], and that implements an IMobility interface that contains basic human motions like walk, run, jump. Lastly create 2 types of Robots with different IMobility implementations.
+ * 4.) Write a typescript class Robot that is generic so that you can
+ * create various Robots of different companies [SpaceX, DynamicIndustries, GenericProtos], and that implements an IMobility interface that contains basic human motions like walk, run, jump. Lastly create 2 types of Robots with different IMobility implementations.
  */
 
 // class here
