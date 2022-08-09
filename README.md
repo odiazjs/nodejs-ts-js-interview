c6a7767ef09bced8d6644654429e121c332264ab
odiazjs <odiaz.dw@gmail.com>
2022-08-09 13:11:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-playground-g96i5e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-playground-g96i5e)
---
+++ index.ts
@@ -0,0 +1,29 @@
+/** HIGHER ORDER FUNCTIONS
+ * 1.) Given a string variable, revert its letter casing and print it to the console.
+ * For example if given the string 'ReVERt this StriNG' the expected output
+ * should be 'rEverT THIS sTRIng'
+ * Conditions: Do not use a for loop or the .forEach utility function,
+ * you must use the .map() array operator.
+ */
+
+const sText = 'LETS INVERT the CasE in THIS sTRING';
+const result = [];
+const invertString = (char) => {
+  // your code here
+};
+
+/** ASYNCHRONOUS PROGRAMMING
+ * 2.) Given a function doAsync(), write code inside this function
+ * that logs to the console a parameter called 'word' from an outside function after 2 seconds
+ */
+
+const doAsync = (/** ? */) => {
+  // your code here
+};
+
+/** OBJECT ORIENTED PROGRAMMING
+ * 3.) Write a typescript class Robot that is generic so that you can
+ * create varios Robots of different companies [SpaceX, DynamicIndustries, GenericProtos], and that implements an IMobility interface that contains basic human motions like walk, run, jump. Lastly create 2 types of Robots with different IMobility implementations.
+ */
+
+// class here
